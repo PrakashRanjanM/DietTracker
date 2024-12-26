a7cac27da654d69ab75c415810f3c5db8d861772
Prakash Ranjan <prakash_ranjan@epam.com>
2024-12-26 14:45:47 +0530

    Basic Project Setup : added service registry and created user management service

---
+++ serviceRegistry/src/test/java/com/diet/tracker/serviceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diet.tracker.serviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
